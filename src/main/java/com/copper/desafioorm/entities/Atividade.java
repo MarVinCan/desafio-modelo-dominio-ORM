a7fdc6d92f6c2d7d82a996fec0e619c1ce57a820
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2023-04-12 14:39:25 -0300

    adição class Participante, interação Atividade com Participante ManyToMany

--- src/main/java/com/copper/desafioorm/entities/Atividade.java
+++ src/main/java/com/copper/desafioorm/entities/Atividade.java
@@ -1,13 +1,15 @@
 package com.copper.desafioorm.entities;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -31,7 +33,13 @@ public class Atividade {
     private Categoria categoria;
 
     @OneToMany(mappedBy = "atividade")
-    private List<Blocos> blocos = new ArrayList<>();
+    private Set<Blocos> blocos = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+            joinColumns = @JoinColumn(name = "atividade_id"),
+            inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
 
     public Atividade(){
 
@@ -85,9 +93,15 @@ public class Atividade {
         this.categoria = categoria;
     }
 
-    public List<Blocos> getBlocos() {
+    public Set<Blocos> getBlocos() {
         return blocos;
     }
+
+    public Set<Participante> getParticipantes() {
+        return participantes;
+    }
+
+ 
 
   
     
